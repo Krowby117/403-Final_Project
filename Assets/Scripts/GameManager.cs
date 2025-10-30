f5762f1ca8a9881541b76bfe21be9df21c75c518
CDees <bcd021@latech.edu>
2025-10-29 21:37:21 -0500

    Add game timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class GameManager : MonoBehaviour
 {
+    // establishing counter for day length 
+    // should remain constant throughout game as quota increases 
+
+    private float dayLength = 60f;
+    private bool countingDown = true; // not intially counting down on main menu 
+    private int seconds; 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +20,25 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Countdown();
+    }
+
+    /// <summary>
+    ///  Countdown function which decreases according to deltatime 
+    ///  only counts down if countingDown is true 
+    /// </summary>
+
+    void Countdown()
+    {
+        if (countingDown) {
+            dayLength -= Time.deltaTime;
+        }
+        // converting to seconds and displaying 
+        int seconds = (int)(dayLength % 60);
+        Debug.Log("Counter is now: " + seconds);
+
+
+
+
     }
 }
